e6a8c15706e7ea3349f8433ef76bb93a0e7152c4
Foster <593301@bah.com>
2017-10-10 12:10:09 -0400

    [TDD] Quick change to server tools for host fix ver. 2

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -4,17 +4,17 @@ from fabric.context_managers import settings
 
 def _get_manage_dot_py(host):
 	# was {host}
-	return f'~/sites/jfoste.gq/virtualenv/bin/python ~/sites/jfoste.gq/source/manage.py'
+	return f'~/sites/{{host}/virtualenv/bin/python ~/sites/{host}/source/manage.py'
 
 
 def reset_database(host):
 	manage_dot_py = _get_manage_dot_py(host)
-	with settings(host_string=f'ubuntu@{host}'):  
+	with settings(host_string=f'ubuntu@ec2-34-226-196-38.compute-1.amazonaws.com'):  
 		run(f'{manage_dot_py} flush --noinput')  
 
 
 def create_session_on_server(host, email):
 	manage_dot_py = _get_manage_dot_py(host)
-	with settings(host_string=f'ubuntu@{host}'):  
+	with settings(host_string=f'ubuntu@ec2-34-226-196-38.compute-1.amazonaws.com'):  
 		session_key = run(f'{manage_dot_py} create_session {email}')  
 		return session_key.strip()
