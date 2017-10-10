8449938f9221406242cdbf082a47be604efd6e77
Foster <593301@bah.com>
2017-10-10 13:10:02 -0400

    [TDD] Quick change to server tools for host fix ver. 6

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,6 +1,7 @@
 from fabric.api import run, env
 from fabric.context_managers import settings
 
+env.user = 'ubuntu'
 env.key_filename = '~/.ssh/django-foster.pem'
 
 def _get_manage_dot_py(host):
@@ -9,12 +10,12 @@ def _get_manage_dot_py(host):
 
 def reset_database(host):
 	manage_dot_py = _get_manage_dot_py(host)
-	with settings(host_string='ec2-34-226-196-38.compute-1.amazonaws.com'):  
+	with settings(host_string=f'{user}@ec2-34-226-196-38.compute-1.amazonaws.com'):  
 		run(f'{manage_dot_py} flush --noinput')  
 
 
 def create_session_on_server(host, email):
 	manage_dot_py = _get_manage_dot_py(host)
-	with settings(host_string='ec2-34-226-196-38.compute-1.amazonaws.com'):  
+	with settings(host_string=f'{user}@ec2-34-226-196-38.compute-1.amazonaws.com'):  
 		session_key = run(f'{manage_dot_py} create_session {email}')  
 		return session_key.strip()
